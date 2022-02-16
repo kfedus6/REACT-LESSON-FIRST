46441b841c00a6d40779b355f2de0abad3798147
kfedus6 <feduskolya@gmail.com>
2022-02-16 14:10:24 +0200

    Sample code

--- timer.jsx
+++
@@ -1,9 +0,0 @@
-function tick() {
-   ReactDOM.render(
-      <div>
-         <h2>Время {new Date().getSeconds()}</h2>
-      </div>,
-      document.getElementById('container')
-   )
-}
-setInterval(tick, 1000);
